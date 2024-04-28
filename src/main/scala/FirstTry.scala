c0c574fe5a9138d488c7525e55096054a2b2beaf
Angel Alvarez Pascua <angel.alvarez@sdggroup.com>
2024-04-28 06:40:56 +0200

    new

--- src/main/scala/FirstTry.scala
+++ src/main/scala/FirstTry.scala
@@ -21,9 +21,13 @@ object FirstTry {
     val bigRDD = spark.sparkContext.parallelize(1 to numRows, numSlices = 2)
       .map(seed => generateRandomRecord(seed))
     val bigDF = bigRDD.toDF("key", "value")
+    println(s"bigDF rowCount: ${bigDF.queryExecution.analyzed.stats.rowCount}")
+    println(s"bigDF sizeInBytes: ${bigDF.queryExecution.analyzed.stats.sizeInBytes}")
 
     // 4. Create the small table dataframe
     val smallDF = spark.createDataFrame(spark.sparkContext.emptyRDD[Row], bigDF.schema)
+    println(s"smallDF rowCount: ${smallDF.queryExecution.analyzed.stats.rowCount}")
+    println(s"smallDF sizeInBytes: ${smallDF.queryExecution.analyzed.stats.sizeInBytes}")
 
     // 5. Join
     val joinDF = bigDF.join(smallDF, "key")
